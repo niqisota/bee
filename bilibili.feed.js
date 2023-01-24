71d7f2f71f379214083232475515a30711f77a7b
nssimida <tomufeng@163.com>
2023-01-24 18:09:02 +0800

    Update bilibili.feed.js

--- bilibili.feed.js
+++ bilibili.feed.js
@@ -4,14 +4,26 @@ if (!$response.body) {
 }
 
 let body = JSON.parse($response.body)
-const blockWords = ['三体', 'b站']
+const blockWords = ['三体', 'b站', '鬼畜']
 
-function isBlock(text = '') {
-  return blockWords.findIndex(w => text.toLowerCase().replace(/\s+/g, '').includes(w)) > -1
+function isBlock(text) {
+  return text && blockWords.findIndex(w => text.toLowerCase().replace(/\s+/g, '').includes(w)) > -1
 }
 
+console.log(`bilibili before filter: ${body.data.items.length}`)
+
 body.data.items = body.data.items.filter(row => {
-  return !(isBlock(row.title) || isBlock(row.talk_back) || (row.args && (isBlock(row.args.up_name) || isBlock(row.args.rname) || isBlock(row.args.tname))))
+  const blocked = isBlock(row.title) || isBlock(row.talk_back) || isBlock(row.args?.up_name) || isBlock(row.args?.rname) || isBlock(row.args?.tname)
+  
+  if (blocked) {
+    console.log(`blocked ${row.title}`)
+  }
+  
+  return !blocked
 })
 
-$done(JSON.stringify(body))
+console.log(`bilibili after filter: ${body.data.items.length}`)
+
+$done({
+  body: JSON.stringify(body)
+})
